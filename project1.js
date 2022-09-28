72bb445d5271abc77859ce4924f0a7e1c33712aa
Ravi chauhan <rcpree6@gmail.com>
2022-09-29 04:39:58 +0530

    first commit

---
+++ project1.js
@@ -0,0 +1,11 @@
+function parents() {
+    console.log("You will be a" , job,    "in"  , location,  "and married to"  ,  partner, "with"  , numkids, "kids" );
+}
+
+let job = ("secretory")
+let location = "delhi"
+let partner = "Raul"
+let numkids = 4
+
+parents();
+
