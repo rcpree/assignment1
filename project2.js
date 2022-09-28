307927c87f1ccef9c06e7002a7aa8ed775b6ac35
rcpree <44681159+rcpree@users.noreply.github.com>
2022-09-29 05:09:28 +0530

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,13 @@
+function compare() {
+    if (num1 === num2) {
+        console.log("number is same")
+    }
+    if (num1 > num2) {
+        console.log(num1)
+    }
+    else { console.log(num2) }
+
+}
+let num1 = 16
+let num2 = 16
+compare();
